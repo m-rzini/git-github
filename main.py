a97bcba176d155d452c3ab37d906864952b6e45c
m-rzini <mohammed.rzini@yahoo.fr>
2026-02-03 11:48:01 +0100

    adding multiplication function

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ def addition(a, b):
 def substraction(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
+
 print(addition(5, 2))
 print(substraction(10, 5))
+print(multiplication(10, 5))
