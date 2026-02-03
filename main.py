561de8427dad665fcce335d495e5c73116fbd2a3
m-rzini <mohammed.rzini@yahoo.fr>
2026-02-03 17:25:27 +0100

    addinf division function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def addition(a, b):
-    return a + b
+    return a + b + 1
 
 def substraction(a, b):
     return a - b
@@ -7,6 +7,10 @@ def substraction(a, b):
 def multiplication(a, b):
     return a * b
 
+def divition(a, b):
+    return a // b
+
 print(addition(5, 2))
 print(substraction(10, 5))
 print(multiplication(10, 5))
+print(divition(8,2))
